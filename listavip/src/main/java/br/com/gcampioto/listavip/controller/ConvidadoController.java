feddfc7d3ab8413f7b3b059c14af9a729464e56b
GaCampioto <gcampioto@gmail.com>
2017-04-12 07:31:20 -0300

    implementando insert e delete de registros

--- listavip/src/main/java/br/com/gcampioto/listavip/controller/ConvidadoController.java
+++ listavip/src/main/java/br/com/gcampioto/listavip/controller/ConvidadoController.java
@@ -5,7 +5,10 @@ import br.com.gcampioto.listavip.repository.ConvidadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -29,5 +32,18 @@ public class ConvidadoController {
 
         model.addAttribute("convidados", convidados);
         return "listaConvidados";
+    }
+
+    @RequestMapping(value = "/salvar", method = RequestMethod.POST)
+    public String salvar(Convidado convidado){
+        convidadoRepository.save(convidado);
+        return "redirect:/listaConvidados";
+    }
+
+    @RequestMapping(value = "/deletar/{id}", method = RequestMethod.DELETE)
+    public String deletar(@PathVariable("id") String id){
+        Long idLong = Long.valueOf(id);
+        convidadoRepository.delete(idLong);
+        return "redirect:/listaConvidados";
     }
 }
